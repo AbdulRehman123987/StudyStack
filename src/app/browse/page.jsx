2cade5da6cfcf4b838e08c5c2cb59bcfd993119a
AbdulRehman123987 <rehmanmirza458@gmail.com>
2025-08-03 20:58:22 +0500

    Slider fixes

--- src/app/browse/page.jsx
+++ src/app/browse/page.jsx
@@ -9,12 +9,16 @@ export default function Browse() {
   const { allNotes } = useContext(NoteContext);
   const scienceNotes = allNotes.filter((note) => note.subject == "science");
   const mathNotes = allNotes.filter((note) => note.subject == "math");
+  const historyNotes = allNotes.filter((note) => note.subject == "history");
+  const computerNotes = allNotes.filter((note) => note.subject == "history");
   return (
     <>
       <AutoCarousel />
       <SearchBar />
       <Slider title={"Science"} Notes={scienceNotes} />
       <Slider title={"Math"} Notes={mathNotes} />
+      <Slider title={"History"} Notes={historyNotes} />
+      <Slider title={"Computer Science"} Notes={computerNotes} />
     </>
   );
 }
